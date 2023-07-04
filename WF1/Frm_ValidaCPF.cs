d3bdc99a26c6097bc19d7996a0fc339259b7d4df
Daniel <danielkaiel10@gmail.com>
2023-07-04 17:54:24 -0300

    novo formulario ValidaCpf2

--- WF1/Frm_ValidaCPF.cs
+++ WF1/Frm_ValidaCPF.cs
@@ -22,44 +22,10 @@ namespace WF1
             Msk_InputCPF.Text = "";
             Lbl_ResultadoValidacao.Text = "";
         }
-        public bool Valida(string cpf)
-        {
-            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            string tempCpf;
-            string digito;
-            int soma;
-            int resto;
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", "");
-            if (cpf.Length != 11)
-                return false;
-            tempCpf = cpf.Substring(0, 9);
-            soma = 0;
-            for (int i = 0; i < 9; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            digito = resto.ToString();
-            tempCpf = tempCpf + digito;
-            soma = 0;
-            for (int i = 0; i < 10; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
-            resto = soma % 11;
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            digito = digito + resto.ToString();
-            return cpf.EndsWith(digito);
-        }
         private void Btn_Validar_Click(object sender, EventArgs e)
         {
             string cpfDigitado = Msk_InputCPF.Text;
-            bool valida = Valida(cpfDigitado);
+            bool valida = Cls_Uteis.Valida(cpfDigitado);
 
             if(valida == false)
             {
