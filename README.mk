9e842b1d996c13855a1b02b7f97dddb0e414ed0a
Daniel <danielkaiel10@gmail.com>
2023-07-06 11:03:08 -0300

    Adicionando o wf1.exe e ReadMe

---
+++ README.mk
@@ -0,0 +1,14 @@
+# DemonstraçãoKey
+Este formulário captura a tecla pressionada, adiciona a caixa de texto e apresenta as letras maíúsculas e minúsculas.
+
+# HelloWorld
+Este formulário troca o Título pela informação digitada no input.
+
+# Máscara
+Este formulário apresenta os tipos principais de máscaras.
+
+# ValidaCPF e ValidaCPF2
+Duas formas de verificar se um CPF é válido.
+
+# Valida Senha
+Este formulário Efetua uam avaliação da senha digitada afim de indicar sua força.
